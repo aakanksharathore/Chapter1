d0ffe973b4e6e5086bc2676d578e08769aeade56
Aakanksha <rathore.aakanksha58@gmail.com>
2018-08-07 11:27:30 +0530

    Code before segregating species wise

--- IMcomparision.R
+++ IMcomparision.R
@@ -9,10 +9,12 @@ head(dat)
 summary(dat)
 
 colnames(dat)
+library("ggplot2")
+library("hexbin")
 
-##Get mean error method wise and species wise
-#out_failed=tapply(dat$failed.percent,list(dat$Method.Name,dat$Species),mean)
-#out_false=tapply(dat$false.percent,list(dat$Method.Name,dat$Species),mean)
+####################################################################################################
+#1. Comparision of error across techniques
+####################################################################################################
 
 lev=paste(dat$Method.Name," ",dat$Species)
 n1=c("turquoise","orange") #1:length(unique(dat$Method.Name))
@@ -29,4 +31,194 @@ means <- tapply(dat$false.percent,lev,mean)
 points(means*100,col="red",pch=9,cex=1)
 
 
-#Graphs for video names
+# Error density map for eachtechnique separately
+##Segmentation
+datN=NA
+datN=dat[which(dat$Method.Name=="Segmentation"),]
+ggplot (datN, aes (x = failed.percent, y = false.percent,colour=datN$Species,main="Segmentation")) + stat_density2d ()
+
+plot(datN$failed.percent, datN$false.percent, pch=23,bg=c("yellow2","cyan3","salmon1")[unclass(datN$Species)],main="Detection errors when using color thresholding")
+par(xpd=TRUE)
+legend(0.85,-0.1,levels(datN$Species), pch = 23,col="black",pt.bg=c("yellow2","cyan3","salmon1"))
+
+##Subtraction
+datN=NA
+datN=dat[which(dat$Method.Name=="Subtraction"),]
+ggplot (datN[which(dat$=="Subtraction"),], aes (x = failed.percent, y = false.percent,colour=datN$Species,main="Subtraction")) + geom_point(data = datN,mapping = aes(x = failed.percent, y = false.percent,colour=datN$Species,shape = datN$Species))+stat_density2d ()
+
+plot(datN$failed.percent, datN$false.percent, pch=23,bg=c("yellow2","cyan3","salmon1")[unclass(datN$Species)],main="Detection errors when using image subtraction")
+par(xpd=TRUE)
+legend(0.85,-0.1,levels(datN$Species), pch = 23,col="black",pt.bg=c("yellow2","cyan3","salmon1"))
+#####################################################################################################3
+#2. Variation inerrors~ video id
+######################################################################################################3
+#Graphs - video id is predictor variable
+
+## For blackbuck
+datN=NA
+datN=dat[which(dat$Species=="Blackbuck"),]
+lev=paste(datN$VideoName,datN$Method.Name)
+
+boxplot((datN$failed.percent*100)~lev,data=datN,ylab="% failed detections",notch=TRUE,col=ifelse(levels(datN$Method.Name)=="Segmentation","turquoise","orange"),main="Blackbuck",xaxt="n")
+axis(1,at=1:length(lev),labels=substr(lev,1,regexpr(' ',lev)),las=2)
+par(xpd=TRUE)
+legend(0.85,-0.1,levels(datN$Method.Name),pch=22,pt.bg=ifelse(levels(datN$Method.Name)=="Segmentation","turquoise","orange"))
+
+boxplot((datN$false.percent*100)~lev,data=datN,ylab="% false detections",notch=TRUE,col=ifelse(levels(datN$Method.Name)=="Segmentation","turquoise","orange"),main="Blackbuck",xaxt="n")
+axis(1,at=1:length(lev),labels=substr(lev,1,regexpr(' ',lev)),las=2)
+legend("topright",levels(datN$Method.Name),pch=22,pt.bg=ifelse(levels(datN$Method.Name)=="Segmentation","turquoise","orange"))
+
+## For Fish 
+datF=dat[which(dat$Species=="Fish"),]
+lev=paste(datF$VideoName,datF$Method.Name)
+
+boxplot((datF$failed.percent*100)~lev,data=datF,ylab="% failed detections",notch=TRUE,col=ifelse(levels(datF$Method.Name)=="Segmentation","turquoise","orange"),main="Fish",xaxt="n")
+axis(1,at=1:length(lev),labels=substr(lev,1,regexpr(' ',lev)),las=2)
+par(xpd=TRUE)
+legend("topright",levels(datF$Method.Name),pch=22,pt.bg=ifelse(levels(datF$Method.Name)=="Segmentation","turquoise","orange"))
+
+boxplot((datF$false.percent*100)~lev,data=datF,ylab="% false detections",notch=TRUE,col=ifelse(levels(datF$Method.Name)=="Segmentation","turquoise","orange"),main="Fish",xaxt="n")
+axis(1,at=1:length(lev),labels=substr(lev,1,regexpr(' ',lev)),las=2)
+par(xpd=TRUE)
+legend("topright",levels(datF$Method.Name),pch=22,pt.bg=ifelse(levels(datF$Method.Name)=="Segmentation","turquoise","orange"))
+
+
+################################################################################################################3
+##2C. Variation in error across all videos all species
+#######################################################################333333333333#########33333333######33
+
+#Same as 1?
+##############################################################################################################
+
+
+#############################################Relation with group and vidoe properties#################################
+
+##Blackbuck-Segmentation
+datN=NA
+datN=dat[which(dat$Species=="Blackbuck" & dat$Method.Name=="Segmentation"),]
+plot(datN$failed.percent~datN$Contrast,pch=23,bg="cyan3")
+par(xpd=TRUE)
+#legend(0.8,-0.1,levels(datN$Method.Name), pch = 23,col="black",pt.bg=c("yellow2","cyan3","salmon1"))
+corCon_BB=cor.test(datN$Contrast,datN$failed.percent)  ##Correlations
+abline(lm(datN$failed.percent~datN$Contrast), col="red")
+mtext(paste(" p-value = ",round(corCon_BB$p.value,3)))
+
+plot(datN$false.percent~datN$Contrast,pch=23,bg="cyan3")
+par(xpd=TRUE)
+corCon_BB=cor.test(datN$Contrast,datN$false.percent)  ##Correlations
+abline(lm(datN$false.percent~datN$Contrast), col="red")
+mtext(paste(" p-value = ",round(corCon_BB$p.value,3)))
+
+#Density
+plot(datN$failed.percent~datN$Narea,pch=23,bg="cyan3")
+par(xpd=TRUE)
+corDen_BB=cor.test(datN$Narea,datN$failed.percent)  ##Correlations
+abline(lm(datN$failed.percent~datN$Narea), col="red")
+mtext(paste(" p-value = ",round(corDen_BB$p.value,3)))
+
+
+plot(datN$false.percent~datN$Narea,pch=23,bg="cyan3")
+par(xpd=TRUE)
+corDen_BB=cor.test(datN$Narea,datN$false.percent)  ##Correlations
+abline(lm(datN$false.percent~datN$Narea), col="red")
+mtext(paste(" p-value = ",round(corDen_BB$p.value,3)))
+
+##Blackbuck-Subtraction
+datN=NA
+datN=dat[which(dat$Species=="Blackbuck" & dat$Method.Name=="Subtraction"),]
+plot(datN$failed.percent~datN$Contrast,pch=23,bg="cyan3")
+par(xpd=TRUE)
+#legend(0.8,-0.1,levels(datN$Method.Name), pch = 23,col="black",pt.bg=c("yellow2","cyan3","salmon1"))
+corCon_BB=cor.test(datN$Contrast,datN$failed.percent)  ##Correlations
+abline(lm(datN$failed.percent~datN$Contrast), col="red")
+mtext(paste(" p-value = ",round(corCon_BB$p.value,3)))
+
+plot(datN$false.percent~datN$Contrast,pch=23,bg="cyan3")
+par(xpd=TRUE)
+#legend(0.8,-0.1,levels(datN$Method.Name), pch = 23,col="black",pt.bg=c("yellow2","cyan3","salmon1"))
+corCon_BB=cor.test(datN$Contrast,datN$false.percent)  ##Correlations
+abline(lm(datN$failed.percent~datN$Contrast), col="red")
+mtext(paste(" p-value = ",round(corCon_BB$p.value,3)))
+
+
+
+#Density
+plot(datN$failed.percent~datN$Narea,pch=23,bg="cyan3")
+par(xpd=TRUE)
+corDen_BB=cor.test(datN$Narea,datN$failed.percent)  ##Correlations
+abline(lm(datN$failed.percent~datN$Narea), col="red")
+mtext(paste(" p-value = ",round(corDen_BB$p.value,3)))
+
+
+plot(datN$false.percent~datN$Narea,pch=23,bg="cyan3")
+par(xpd=TRUE)
+corDen_BB=cor.test(datN$Narea,datN$false.percent)  ##Correlations
+abline(lm(datN$false.percent~datN$Narea), col="red")
+mtext(paste(" p-value = ",round(corDen_BB$p.value,3)))
+
+##Fish-Segmentation
+datN=NA
+datN=dat[which(dat$Species=="Fish" & dat$Method.Name=="Segmentation"),]
+plot(datN$failed.percent~datN$Contrast,pch=23,bg="cyan3")
+par(xpd=TRUE)
+#legend(0.8,-0.1,levels(datN$Method.Name), pch = 23,col="black",pt.bg=c("yellow2","cyan3","salmon1"))
+corCon_BB=cor.test(datN$Contrast,datN$failed.percent)  ##Correlations
+abline(lm(datN$failed.percent~datN$Contrast), col="red")
+mtext(paste(" p-value = ",round(corCon_BB$p.value,3)))
+
+plot(datN$false.percent~datN$Contrast,pch=23,bg="cyan3")
+par(xpd=TRUE)
+corCon_BB=cor.test(datN$Contrast,datN$false.percent)  ##Correlations
+abline(lm(datN$false.percent~datN$Contrast), col="red")
+mtext(paste(" p-value = ",round(corCon_BB$p.value,3)))
+
+#Density
+plot(datN$failed.percent~datN$Area,pch=23,bg="cyan3")
+par(xpd=TRUE)
+corDen_BB=cor.test(datN$Area,datN$failed.percent)  ##Correlations
+abline(lm(datN$failed.percent~datN$Area), col="red")
+mtext(paste(" p-value = ",round(corDen_BB$p.value,3)))
+
+
+plot(datN$false.percent~datN$Area,pch=23,bg="cyan3")
+par(xpd=TRUE)
+corDen_BB=cor.test(datN$Area,datN$false.percent)  ##Correlations
+abline(lm(datN$false.percent~datN$Area), col="red")
+mtext(paste(" p-value = ",round(corDen_BB$p.value,3)))
+
+##Fish-Subtraction
+datN=NA
+datN=dat[which(dat$Species=="Fish" & dat$Method.Name=="Subtraction"),]
+plot(datN$failed.percent~datN$Contrast,pch=23,bg="cyan3")
+par(xpd=TRUE)
+#legend(0.8,-0.1,levels(datN$Method.Name), pch = 23,col="black",pt.bg=c("yellow2","cyan3","salmon1"))
+corCon_BB=cor.test(datN$Contrast,datN$failed.percent)  ##Correlations
+abline(lm(datN$failed.percent~datN$Contrast), col="red")
+mtext(paste(" p-value = ",round(corCon_BB$p.value,3)))
+
+plot(datN$false.percent~datN$Contrast,pch=23,bg="cyan3")
+par(xpd=TRUE)
+#legend(0.8,-0.1,levels(datN$Method.Name), pch = 23,col="black",pt.bg=c("yellow2","cyan3","salmon1"))
+corCon_BB=cor.test(datN$Contrast,datN$false.percent)  ##Correlations
+abline(lm(datN$failed.percent~datN$Contrast), col="red")
+mtext(paste(" p-value = ",round(corCon_BB$p.value,3)))
+
+
+
+#Density
+plot(datN$failed.percent~datN$Area,pch=23,bg="cyan3")
+par(xpd=TRUE)
+corDen_BB=cor.test(datN$Area,datN$failed.percent)  ##Correlations
+abline(lm(datN$failed.percent~datN$Area), col="red")
+mtext(paste(" p-value = ",round(corDen_BB$p.value,3)))
+
+
+plot(datN$false.percent~datN$Area,pch=23,bg="cyan3")
+par(xpd=TRUE)
+corDen_BB=cor.test(datN$Area,datN$false.percent)  ##Correlations
+abline(lm(datN$false.percent~datN$Area), col="red")
+mtext(paste(" p-value = ",round(corDen_BB$p.value,3)))
+
+
+
+
